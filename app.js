f4891704b40e1cdd22e360bdcabf3c03cb840283
Himanshu Paithane <himanshupaithane@Himanshus-MacBook-Air.local>
2025-04-01 17:27:44 -0400

    db connection and auth setup done

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-import configRoutesFunction from "./routes/index.js";
+// import express from "express";
+// import path from "path";
+// import { fileURLToPath } from "url";
+// import configRoutesFunction from "./routes/index.js";
+
+// const app = express();
+
+// // Fix __dirname issue in ES Modules
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// app.use(express.static(path.join(__dirname, "public")));
+// configRoutesFunction(app);
+
+// const PORT = process.env.PORT || 3000;
+// console.log("We've now got a server!");
+// app.listen(PORT, () => console.log("Server is running on http://localhost:3000"));
+
+
+import express from 'express';
+import connectDB from './config/mongoConnections.js';
+import dotenv from 'dotenv';
+import authRoutes from './routes/authRoutes.js';
+
+
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
 
-// Fix __dirname issue in ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+connectDB(); // connect to MongoDB here
 
-app.use(express.static(path.join(__dirname, "public")));
-configRoutesFunction(app);
 
+// Routes
+app.use('/api/auth', authRoutes);
+
+// routes...
 const PORT = process.env.PORT || 3000;
-console.log("We've now got a server!");
-app.listen(PORT, () => console.log("Server is running on http://localhost:3000"));
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
