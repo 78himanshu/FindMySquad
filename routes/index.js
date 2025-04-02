85c89cab05e4481a0b1fbdfa55c1d52dc46277e7
adityadav1220 <ayadav25@stevens.edu>
2025-04-02 02:12:52 -0400

    Host Game -- Creation
    Implemented Host Game backend functionality

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 import landingRoutes from "./landingRoutes.js";
+import hostGamesRoutes from "./hostGamesRoutes.js"
+import authRoutes from './authRoutes.js';
 
 const configRoutesFunction = (app) => {
     app.use("/", landingRoutes);
+    app.use('/host', hostGamesRoutes);
+    app.use('/api/auth', authRoutes);
 };
 
 export default configRoutesFunction;
