99cca30cfa5ddf9f2828509cb6eedac77ebdeedf
adityadav1220 <ayadav25@stevens.edu>
2025-04-02 12:24:43 -0400

    Added controllers-hostGameControllers.js content
    Added controllers file of hostGameControllers to do all the error checking and then calling in routes

--- routes/hostGamesRoutes.js
+++ routes/hostGamesRoutes.js
@@ -1,52 +1,9 @@
-import express from 'express';
-import {Router} from 'express';
-const router = Router();
-import {hostGameData} from '../data/index.js'
-import { checkString,checkNumber } from '../utils/helper.js';
+import express from 'express'
+import {createGameController } from "../controllers/hostGameControllers.js";
+
+const router = express.Router();
 
 router
-    .route('/')
-    .post(async(req,res) =>{
-        const x = req.body;
-        if (!x || Object.keys(x).length === 0){
-            return res
-            .status(400)
-            .json({error: 'There are no fields in the request body'});
-        }
-        if( !x.title || !x.sport || !x.venue || !x.playersRequired || !x.dateTime || !x.costPerHead || !x.skillLevel || !x.host || !x.location){
-            return res
-            .status(400)
-            .json({error: 'All fields need to have valid values'});
-        }
-        try {
-            checkString(x.title,'Title')
-            checkString(x.sport,'Sport')
-            checkString(x.venue,'Venue')
-            checkString(x.skillLevel,'Skill Level')
-            checkString(x.location,'Location')
-            checkString(x.description,'Description')
-            checkNumber(x.playersRequired,'Players Required',1)
-            checkNumber(x.costPerHead,'Cost per Head',0)
-        } catch (e) {
-            return res.status(400).json({ error: e.message });
-        }
-        try {
-            const newGame = await hostGameData.createGame(
-                x.title,
-                x.sport,
-                x.venue,
-                x.playersRequired,
-                x.dateTime,
-                x.description,
-                x.costPerHead,
-                x.skillLevel,
-                x.host,
-                x.location
-            )
-            res.status(200).json(newGame);
-        } catch (e) {
-            res.status(400).json({error: e.message})
-        }
-    })
+    .post('/', createGameController);
 
-    export default router;
+export default router;
